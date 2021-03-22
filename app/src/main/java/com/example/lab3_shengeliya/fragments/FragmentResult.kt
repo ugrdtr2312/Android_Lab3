928e24a5e83d0f9c7b5057ecea67919cf4a5847c
Shenheliia Volodymyr <vladimir231200@gmail.com>
2021-03-22 20:48:38 +0200

    FragmentResult updated

--- app/src/main/java/com/example/lab3_shengeliya/fragments/FragmentResult.kt
+++ app/src/main/java/com/example/lab3_shengeliya/fragments/FragmentResult.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.lab3_shengeliya.R
-import com.example.lab3_shengeliya.database.DatabaseHandler
 
 class FragmentResult : Fragment() {
 
